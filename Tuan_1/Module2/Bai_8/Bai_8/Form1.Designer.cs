4f5e586045ad61c77cfead6582aa7051bdd0ac8c
letranbaosuong <letranbaosuong@gmail.com>
2020-03-13 22:19:14 +0700

    update Tuần 1 - Module 2 - bài 8

--- Tuan_1/Module2/Bai_8/Bai_8/Form1.Designer.cs
+++ Tuan_1/Module2/Bai_8/Bai_8/Form1.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnTop = new System.Windows.Forms.Panel();
+            this.pnBottom = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.pnButton = new System.Windows.Forms.Panel();
+            this.pnTop.SuspendLayout();
+            this.pnBottom.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnTop
+            // 
+            this.pnTop.Controls.Add(this.lblStatus);
+            this.pnTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnTop.Location = new System.Drawing.Point(0, 0);
+            this.pnTop.Name = "pnTop";
+            this.pnTop.Size = new System.Drawing.Size(454, 45);
+            this.pnTop.TabIndex = 0;
+            // 
+            // pnBottom
+            // 
+            this.pnBottom.Controls.Add(this.label1);
+            this.pnBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnBottom.Location = new System.Drawing.Point(0, 314);
+            this.pnBottom.Name = "pnBottom";
+            this.pnBottom.Size = new System.Drawing.Size(454, 47);
+            this.pnBottom.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A Button Stack";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(7, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(292, 26);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Status: Element 4  is clicked.";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 279);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(367, 279);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // pnButton
+            // 
+            this.pnButton.Location = new System.Drawing.Point(137, 51);
+            this.pnButton.Name = "pnButton";
+            this.pnButton.Size = new System.Drawing.Size(200, 251);
+            this.pnButton.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(454, 361);
+            this.Controls.Add(this.pnButton);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.pnBottom);
+            this.Controls.Add(this.pnTop);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.pnTop.ResumeLayout(false);
+            this.pnTop.PerformLayout();
+            this.pnBottom.ResumeLayout(false);
+            this.pnBottom.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnTop;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Panel pnBottom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Panel pnButton;
     }
 }
 
--- Tuan_1/Module2/Bai_8/Bai_8/Form1.cs
+++ Tuan_1/Module2/Bai_8/Bai_8/Form1.cs
@@ -12,9 +12,25 @@ namespace Bai_8
 {
     public partial class Form1 : Form
     {
+        int count = -1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            count++;
+            Button newButton = new Button();
+            newButton.Text = "Element " + count.ToString();
+            newButton.Click += new System.EventHandler(Button_Click);
+            pnButton.Controls.Add(newButton);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            lblStatus.Text = "Status: " + button.Text + "  is clicked.";
         }
     }
 }
