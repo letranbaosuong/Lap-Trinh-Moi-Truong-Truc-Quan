29586a162b0b9f938850c030dace567ff79a1645
letranbaosuong <letranbaosuong@gmail.com>
2020-04-08 17:14:06 +0700

    connected LinQ to PostgreSQL

---
+++ Tuan_6/ConsoleAppLinQPostgreSQL/ConsoleAppLinQPostgreSQL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbCrudContext;
+
+namespace ConsoleAppLinQPostgreSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DbCrudDataContext context = new DbCrudDataContext())
+            {
+                var query = from crud in context.Cruds
+                            orderby crud.Id
+                            select crud;
+
+                foreach (Crud crud in query)
+                {
+                    Console.WriteLine("{0} | {1} | {2} | {3}", crud.Id, crud.FirstName, crud.LastName, crud.Gender);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
